d2ee95d20fa76ea130f53adf6a87079965917e21
asanchez8801 <jasanchez8801@gmail.com>
2024-05-17 19:55:34 -0500

    form

--- Proyecto-Angular-4/src/app/components/iniciar-sesion/iniciar-sesion.component.ts
+++ Proyecto-Angular-4/src/app/components/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-iniciar-sesion',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './iniciar-sesion.component.html',
   styleUrl: './iniciar-sesion.component.css'
 })
 export class IniciarSesionComponent {
 
+  formularioCredenciales = new FormGroup({
+    user: new FormControl(""),
+    password: new FormControl(""),
+  })
+  manejarEnvio(){
+    console.log("formularioCredenciales: ", this.formularioCredenciales.value);
+  }
 }
